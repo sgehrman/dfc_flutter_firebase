7d34f2417bfac351a6b74671da9f227c9cd32f00
Steve Gehrman <sgehrman@gmail.com>
2022-06-27 19:32:44 -0700

    fix

--- lib/dfc_flutter_firebase_web.dart
+++ lib/dfc_flutter_firebase_web.dart
@@ -1,11 +1,16 @@
 library dfc_flutter_firebase_web;
 
+export 'src/chat/chat_models.dart';
+export 'src/chat/chat_screen.dart';
+export 'src/chat/widgets/avatar_container.dart';
 export 'src/firebase/auth.dart';
 export 'src/firebase/firebase_user_provider.dart';
 export 'src/firebase/firebase_utils.dart';
 export 'src/firebase/firestore.dart';
 export 'src/firebase/firestore_refs.dart';
 export 'src/firebase/serializable.dart';
+export 'src/image/image_delete_dialog.dart';
+export 'src/image/image_manager.dart';
 export 'src/image/image_url_model.dart';
 export 'src/login/phone_verifier.dart';
 export 'src/login/user_login_screen.dart';
